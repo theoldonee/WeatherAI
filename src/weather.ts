a758a88b3a2b135c5781ae82da8ed597b143cc2d
Mastermoh0 <mkmdalamin2005@gmail.com>
2026-06-16 01:17:44 +0400

    feat: implement Weather.GetForcast stub

--- src/weather.ts
+++ src/weather.ts
@@ -1,4 +1,12 @@
-export class Weather{
+import { Weather as WeatherInterface, WeatherData } from "./lib/services/shared-interfaces";
 
-    
+export class Weather implements WeatherInterface {
+    GetForcast(_longitiude: number, _latitude: number): WeatherData {
+        // Return structured weather data
+        return {
+            probabilityOfRain: 25,
+            humidity: 60,
+            temp: 21,
+        };
+    }
 }
